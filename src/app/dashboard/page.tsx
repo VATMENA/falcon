1a7f4c7df644d6f2883b401333935e5c5e0af24f
Samuel Hepworth <samhep0803@gmail.com>
2023-08-09 02:14:18 +0400

    use edge function for main page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,6 +1,8 @@
 import { getDivisionMembers } from "@/app/dashboard/(main-page)/actions";
 import { MembershipTable } from "@/app/dashboard/(main-page)/membership-table";
 
+export const runtime = "edge";
+
 export default async function Dashboard() {
   return (
     <>
