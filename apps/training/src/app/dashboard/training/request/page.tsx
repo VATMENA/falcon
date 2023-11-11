1563d4577cc9933e5559427e3eab3f0f80d5865c
Samuel Hepworth <samhep0803@gmail.com>
2023-11-11 16:12:39 +0400

    start work on training requests

---
+++ apps/training/src/app/dashboard/training/request/page.tsx
@@ -0,0 +1,9 @@
+import { WarningDialog } from "./warning-dialog";
+
+export default function TrainingRequestsPage() {
+  return (
+    <div className="flex flex-col">
+      <WarningDialog />
+    </div>
+  );
+}
--- apps/training/src/components/side-bar.tsx
+++ apps/training/src/components/side-bar.tsx
@@ -1,6 +1,10 @@
 "use client";
 
-import { HamburgerMenuIcon, HomeIcon } from "@radix-ui/react-icons";
+import {
+  CalendarIcon,
+  HamburgerMenuIcon,
+  HomeIcon,
+} from "@radix-ui/react-icons";
 import type { IconProps } from "@radix-ui/react-icons/dist/types";
 import { checkPermissions } from "auth/utils/check-permission";
 import { DivisionRole, SubdivisionRole } from "db";
@@ -22,12 +26,21 @@ interface NavItem {
   subdivisionRole?: SubdivisionRole;
 }
 
-const navItems: NavItem[] = [
+const homeNavItems: NavItem[] = [
   {
     title: "Home",
     href: "/dashboard",
     segment: null,
     icon: HomeIcon,
+  },
+];
+
+const trainingNavItems: NavItem[] = [
+  {
+    title: "Training Request",
+    href: "/dashboard/training/request",
+    segment: "request",
+    icon: CalendarIcon,
   },
 ];
 
@@ -63,32 +76,61 @@ export const MobileNav: React.FC<{ user: User }> = ({ user }) => {
         </Button>
       </SheetTrigger>
       <SheetContent side={"left"} className="w-[300px]">
-        <div className="flex flex-col gap-y-1 pt-4">
-          {navItems.map((navItem, idx) => {
-            if (
-              navItem.divisionRole &&
-              !checkPermissions(
-                "division",
-                navItem.divisionRole,
-                user.divisionRole,
+        <div className="flex flex-col gap-y-4">
+          <div className="flex flex-col gap-y-1 pt-4">
+            {homeNavItems.map((navItem, idx) => {
+              if (
+                navItem.divisionRole &&
+                !checkPermissions(
+                  "division",
+                  navItem.divisionRole,
+                  user.divisionRole,
+                )
               )
-            )
-              return null;
+                return null;
 
-            if (
-              navItem.subdivisionRole &&
-              !checkPermissions(
-                "subdivision",
-                navItem.subdivisionRole,
-                user.subdivisionRole,
+              if (
+                navItem.subdivisionRole &&
+                !checkPermissions(
+                  "subdivision",
+                  navItem.subdivisionRole,
+                  user.subdivisionRole,
+                )
               )
-            )
-              return null;
+                return null;
 
-            return (
-              <NavItemLink key={idx} navItem={navItem} segment={segment} />
-            );
-          })}
+              return (
+                <NavItemLink key={idx} navItem={navItem} segment={segment} />
+              );
+            })}
+          </div>
+          <div className="flex flex-col gap-y-1 pt-4">
+            {trainingNavItems.map((navItem, idx) => {
+              if (
+                navItem.divisionRole &&
+                !checkPermissions(
+                  "division",
+                  navItem.divisionRole,
+                  user.divisionRole,
+                )
+              )
+                return null;
+
+              if (
+                navItem.subdivisionRole &&
+                !checkPermissions(
+                  "subdivision",
+                  navItem.subdivisionRole,
+                  user.subdivisionRole,
+                )
+              )
+                return null;
+
+              return (
+                <NavItemLink key={idx} navItem={navItem} segment={segment} />
+              );
+            })}
+          </div>
         </div>
       </SheetContent>
     </Sheet>
@@ -99,9 +141,34 @@ export const Sidebar: React.FC<{ user: User }> = ({ user }) => {
   const segment = useSelectedLayoutSegment();
 
   return (
-    <div className="hidden md:flex flex-col h-full min-w-[300px] border-r px-8 p-4 gap-y-4">
+    <div className="hidden md:flex flex-col h-full min-w-[300px] border-r p-8 gap-y-4">
       <div className="flex flex-col gap-y-1">
-        {navItems.map((navItem, idx) => {
+        {homeNavItems.map((navItem, idx) => {
+          if (
+            navItem.divisionRole &&
+            !checkPermissions(
+              "division",
+              navItem.divisionRole,
+              user.divisionRole,
+            )
+          )
+            return null;
+
+          if (
+            navItem.subdivisionRole &&
+            !checkPermissions(
+              "subdivision",
+              navItem.subdivisionRole,
+              user.subdivisionRole,
+            )
+          )
+            return null;
+
+          return <NavItemLink key={idx} navItem={navItem} segment={segment} />;
+        })}
+      </div>
+      <div className="flex flex-col gap-y-1 pt-4">
+        {trainingNavItems.map((navItem, idx) => {
           if (
             navItem.divisionRole &&
             !checkPermissions(
